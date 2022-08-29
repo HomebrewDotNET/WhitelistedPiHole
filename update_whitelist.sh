eca44c06b9dcfa8ec179c605d50d935e6e39606e
Jens Sels <jenssels1998@hotmail.com>
2022-08-29 09:41:11 +0200

    Caching of scripts + fix for breaking change of s6-overlay

--- update_whitelist.sh
+++ update_whitelist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Setup constants
-TempDir="/tmp/whitelist_update"
-ScriptDir="$TempDir/whitelist/scripts"
+SourceDir="/src/whitelist_update"
+ScriptDir="$SourceDir/whitelist/scripts"
 WhitelistScript="$ScriptDir/whitelist.py"
 ReferralScript="$ScriptDir/referral.sh"
 GitRepo="https://github.com/anudeepND/whitelist.git"
@@ -10,21 +10,23 @@ GitRepo="https://github.com/anudeepND/whitelist.git"
 #Start
 echo "[$(date)] Updating whitelist for pihole"
 
-mkdir -p $TempDir
+mkdir -p $SourceDir
 
-echo "[$(date)] Pulling latest whitelist scripts"
-git clone $GitRepo $TempDir
+if [ -z "$(ls -A /path/to/dir)" ]; then
+   echo "[$(date)] Pulling latest whitelist scripts"
+   git clone $GitRepo $SourceDir
+else
+   echo "[$(date)] Updating to latest whitelist scripts"
+   git -C $SourceDir pull origin master
+fi
+
 
 echo "[$(date)] Pulled latest whitelist scripts. Updating now"
 python3 $WhitelistScript --docker
-
 
 if [ ! -z $UPDATE_REFERRAL ]; then
 	echo "[$(date)] Updating white using referral list"
 	/bin/bash $ReferralScript --docker
 fi
-
-echo "[$(date)] Cleaning up"
-rm -Rfv $TempDir
 
 echo "[$(date)] Updated whitelist for pihole"
